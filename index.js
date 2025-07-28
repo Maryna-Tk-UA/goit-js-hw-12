b9d26869926e430052c32c6d7308a93b18eb7373
Maryna-Tk-UA <marynapanchenko19@gmail.com>
2025-07-28 00:01:15 +0000

    Deploying to gh-pages from @ Maryna-Tk-UA/goit-js-hw-12@fbbaf166a892ac9f4e15dc932f066adc037a31fe 🚀

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-/* empty css                      */(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+import{a as g,i as l,S as u}from"./assets/vendor-ChKhXQjG.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&a(i)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function a(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const h="51359402-e1cf81f867165d4b6bb985455",p="https://pixabay.com/api/";function d(s){const r={key:h,q:s,image_type:"photo",orientation:"horizontal",safesearch:!0};return g.get(p,{params:r}).then(o=>o.data).catch(o=>{l.error({message:"Ooooops! Something went wrong",position:"topRight",backgroundColor:"#ef4040",progressBar:!1,messageColor:"white",icon:"",iconUrl:new URL("../img/error.svg",import.meta.url).href,close:!1})})}const n=document.querySelector(".gallery"),y=new u(".gallery a"),c=document.querySelector(".container-loader");function w(s){const r=s.map(({webformatURL:o,largeImageURL:a,tags:e,likes:t,views:i,comments:m,downloads:f})=>`
+          <a href="${a}" class="gallery-item">
+          <div class="img-container">
+        <img src="${o}" alt="${e}" class="gallery-item-img" width="360">
+        </div>
+        <ul class="descr-list">
+          <li class="descr-list-item">
+            <h3 class="title-item">Likes</h3>
+            <p class="text-item">${t}</p>
+          </li>
+          <li class="descr-list-item">
+            <h3 class="title-item">Views</h3>
+            <p class="text-item">${i}</p>
+          </li>
+          <li class="descr-list-item">
+            <h3 class="title-item">Comments</h3>
+            <p class="text-item">${m}</p>
+          </li>
+          <li class="descr-list-item">
+            <h3 class="title-item">Downloads</h3>
+            <p class="text-item">${f}</p>
+          </li>
+        </ul>
+      </a>
+    `).join("");n.insertAdjacentHTML("beforeend",r),y.refresh()}function L(){n.innerHTML=""}function b(){c.classList.add("is-active")}function v(){c.classList.remove("is-active")}const C=document.querySelector(".form");C.addEventListener("submit",S);function S(s){s.preventDefault();const r=s.target.elements["search-text"].value.trim();if(r===""){l.error({message:"The search field cannot be empty",position:"topRight",backgroundColor:"#ef4040",progressBar:!1,messageColor:"white",icon:"",iconUrl:new URL("./img/error.svg",import.meta.url).href,close:!1});return}L(),b(),d(r).then(o=>{o.hits.length===0&&l.warning({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",backgroundColor:"#ef4040",progressBar:!1,messageColor:"white",icon:"",iconUrl:new URL("./img/error.svg",import.meta.url).href,close:!1}),w(o.hits)}).catch(o=>{l.error({message:"Ooooops! Something went wrong",position:"topRight",backgroundColor:"#ef4040",progressBar:!1,messageColor:"white",icon:"",iconUrl:new URL("./img/error.svg",import.meta.url).href,close:!1})}).finally(()=>{v()})}
 //# sourceMappingURL=index.js.map
